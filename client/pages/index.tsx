13bd5804dcc28ddeeadd5a92dc7a2495558a895f
Saurav Sah <sah_64@yahoo.com>
2022-07-06 14:26:31 -0400

    added formik hook

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -2,8 +2,19 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import { useFormik } from "formik";
 
 const Home: NextPage = () => {
+	const formik = useFormik({
+		initialValues: {
+			username: "",
+			password: "",
+		},
+		onSubmit: (values) => {
+			alert(JSON.stringify(values, null, 2));
+		},
+	});
+
 	return (
 		<div className={styles.container}>
 			<Head>
@@ -15,11 +26,23 @@ const Home: NextPage = () => {
 			<main className={styles.main}>
 				<h1 className="header">Log In</h1>
 				<div className={styles.formContainer}>
-					<form className={styles.form}>
+					<form className={styles.form} onSubmit={formik.handleSubmit}>
 						<label htmlFor="username">Username</label>
-						<input type="text" id="username" name="username" />
+						<input
+							type="text"
+							id="username"
+							name="username"
+							onChange={formik.handleChange}
+							value={formik.values.username}
+						/>
 						<label htmlFor="password">Password</label>
-						<input type="password" id="password" name="password" />
+						<input
+							type="password"
+							id="password"
+							name="password"
+							onChange={formik.handleChange}
+							value={formik.values.password}
+						/>
 						<button type="submit">Log In</button>
 					</form>
 				</div>
